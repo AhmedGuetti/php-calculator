9c74dcd9de7cb5abba47f3734e919f799aacf261
Ahmed Guetti <guetti4ahmed@gmail.com>
2020-11-04 22:54:59 +0100

    add html

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP calculator</title>
+</head>
+<body>
+    <form>
+        <input type="text" name='numb1' placeholder='Number 1'>
+        <input type="text" name='numb2' placeholder='Number 2'>
+        <select name="operator">
+            <option>Addition</option>
+            <option>Substraction</option>
+            <option>Multiplication</option>
+            <option>Division</option>
+        </select>
+    </form>
+
+    
+</body>
+</html>
